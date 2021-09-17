a26fcb9d02893e2469d79b5fbfd88b0096bc4f9d
slashplus <sp@slashplus.de>
2021-09-17 15:29:03 +0200

    docs(): Add phpdoc blocks

--- src/Validation/ValidatorFactory.php
+++ src/Validation/ValidatorFactory.php
@@ -9,18 +9,56 @@ use Illuminate\Filesystem\Filesystem;
 use Illuminate\Translation\FileLoader;
 use Illuminate\Translation\Translator;
 
+/**
+ * Class ValidatorFactory
+ *
+ * @package Slashplus\IdentityGermany\Validation
+ *
+ * @method make(array $data, array $rules, array $messages = [], array $customAttributes = [])
+ *
+ * @see \Illuminate\Validation\Factory
+ */
 class ValidatorFactory
 {
+    /**
+     * @var mixed|string
+     */
     public $lang;
+
+    /**
+     * @var mixed|string
+     */
     public $group;
+
+    /**
+     * @var Factory
+     */
+
     public $factory;
+    /**
+     * @var mixed|string
+     */
     public $namespace;
 
-    // Translations root directory
+    /**
+     * Translations root directory
+     *
+     * @var string
+     */
     public $basePath;
 
+    /**
+     * @var
+     */
     public static $translator;
 
+    /**
+     * ValidatorFactory constructor.
+     *
+     * @param string $namespace
+     * @param string $lang
+     * @param string $group
+     */
     public function __construct($namespace = 'lang', $lang = 'en', $group = 'validation')
     {
         $this->lang = $lang;
@@ -30,6 +68,10 @@ class ValidatorFactory
         $this->factory = new Factory($this->loadTranslator());
     }
 
+    /**
+     * @param string $path
+     * @return $this
+     */
     public function translationsRootPath(string $path = '')
     {
         if (!empty($path)) {
@@ -39,25 +81,39 @@ class ValidatorFactory
         return $this;
     }
 
+    /**
+     * @return $this
+     */
     private function reloadValidatorFactory()
     {
         $this->factory = new Factory($this->loadTranslator());
         return $this;
     }
 
-    public function getTranslationsRootPath() : string
+    /**
+     * @return string
+     */
+    public function getTranslationsRootPath(): string
     {
-        return dirname(__FILE__) . '/../';
+        return dirname(__FILE__).'/../';
     }
 
-    public function loadTranslator() : Translator
+    /**
+     * @return Translator
+     */
+    public function loadTranslator(): Translator
     {
-        $loader = new FileLoader(new Filesystem(), $this->basePath . $this->namespace);
-        $loader->addNamespace($this->namespace, $this->basePath . $this->namespace);
+        $loader = new FileLoader(new Filesystem(), $this->basePath.$this->namespace);
+        $loader->addNamespace($this->namespace, $this->basePath.$this->namespace);
         $loader->load($this->lang, $this->group, $this->namespace);
         return static::$translator = new Translator($loader, $this->lang);
     }
 
+    /**
+     * @param $method
+     * @param $args
+     * @return false|mixed
+     */
     public function __call($method, $args)
     {
         return call_user_func_array([$this->factory, $method], $args);
