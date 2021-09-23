bfdc5e537c0897f4d44c860f3c97869a24e2ea5b
slashplus <sp@slashplus.de>
2021-09-23 22:12:19 +0200

    feat(): Add more tests for residence permission method

--- tests/AgeVerificationTest.php
+++ tests/AgeVerificationTest.php
@@ -5,6 +5,7 @@ namespace Slashplus\IdentityGermany\Tests;
 use Slashplus\IdentityGermany\AgeVerification;
 use Slashplus\IdentityGermany\Validation\IdCardValidation\Validator as IdCardValidator;
 use Slashplus\IdentityGermany\Validation\PassportValidation\Validator as PassportValidator;
+use Slashplus\IdentityGermany\Validation\ResidenceValidation\Validator as ResidenceValidator;
 
 it('creates a IdCardValidator', function () {
     expect(AgeVerification::create([], 'id_card'))->toBeInstanceOf(IdCardValidator::class);
@@ -12,6 +13,10 @@ it('creates a IdCardValidator', function () {
 
 it('creates a PassportValidator', function () {
     expect(AgeVerification::create([], 'passport'))->toBeInstanceOf(PassportValidator::class);
+});
+
+it('creates a ResidenceValidator', function () {
+    expect(AgeVerification::create([], 'residence'))->toBeInstanceOf(ResidenceValidator::class);
 });
 
 it('throws exception if validator is unknown', function () {
@@ -26,4 +31,4 @@ it('throws no exception if validator type is known', function ($type) {
     }catch (\Exception $e){
         $this->fail("Exception has been thrown: {$e->getMessage()}" );
     }
-})->with(['id_card', 'passport']);
+})->with(['id_card', 'passport', 'residence']);
